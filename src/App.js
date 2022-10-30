6b767770157d1a3d4cc797ba4e5882078c6cfb8d
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-30 14:20:28 +0100

    Outlet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './routes/home/home.component';
 
 
+const Shop =()=>{
+  return <h2> i am the shop page</h2>
+}
+
 
 const App = () => {
 
   return(
     <Routes>
-      <Route path='/home' element={<Home/>}/>
-      
+      <Route path='/home' element={<Home/>}>
+        <Route path='shop' element={<Shop/>}/>
+      </Route>
     </Routes>
   )
 };
