a316f37c0df6430f0e3394abdd66760265f65ab5
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-11-08 18:16:02 +0100

    updated

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,14 +22,15 @@ const firebaseConfig = {
   // Initialize Firebase
   const firebassApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
 
-  provider.setCustomParameters({
+  googleProvider.setCustomParameters({
     prompt :'select_account'
   })
 
 export const auth = getAuth();
-export const signInGooglePopup = () => signInWithPopup(auth,provider)
+export const signInWithGooglePopup = () => signInWithGooglePopup(auth,googleProvider)
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth,googleProvider)
 
 export const db = getFirestore()
 
