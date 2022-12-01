04a962e71aadab1ffb1b0fdd3e43c7f892e8aeb3
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-12-01 21:12:01 +0100

    updated

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,11 +1,21 @@
 import { createContext, useState, useEffect } from "react";
 
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
 
   const value = { products };
   console.log(products);
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 
 import {getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut,onAuthStateChanged} from 'firebase/auth'
 
-import {getFirestore, doc ,getDoc ,setDoc, collection, writeBatch } from 'firebase/firestore'
+import {getFirestore, doc ,getDoc ,setDoc, collection, writeBatch , query, getDocs} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -43,6 +43,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     await bathc.commit();
     console.log('done');
 }
+
+export const getCategoriesAndDocuments = async ()=>{
+  const collectionRef = collection(db, 'categories') ; 
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q)
+  const categoryMap = querySnapshot.docs.reduce((acc,docSnapshot)=>{
+    const {title,items} = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  },{})
+
+  return categoryMap;
+ }
+
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) =>{
   if(!userAuth) return ;
