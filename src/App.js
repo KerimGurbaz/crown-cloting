3781b9364435e95b2b52fe09d4e154002a69662c
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-01-20 00:29:03 +0100

    reducer v-3

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom';
 
-
-
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
-} from "../utils/firebase/firebase.utils";
+} from "./utils/firebase/firebase.utils"
 
 import Authentication from './routes/authentication/authentication.component';
-
-
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component'
-import { useEffect } from 'react';
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
+// import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase/firebase.utils';
 import { setCurrentUser } from "./store/user/user.action";
 
 
