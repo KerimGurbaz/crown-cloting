a20b647049b6cd4e10cb8a8d254b5bacd0dd8492
KerimGurbaz <kerim.grbz.01@gmail.com>
2023-01-23 23:59:42 +0100

    v-3

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,6 +2,9 @@ import { combineReducers } from "redux";
 
 import { useReducer, userReducer } from './user/user.reducer'
 
+import {categoriesReducer} from './user/user.reducer'
+
 export const rootReducer = combineReducers({
     user: userReducer,
+    categories:categoriesReducer,
 })
